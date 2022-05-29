039ffc88ec3ea9807ff2e29444032b09a809b6d0
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-05-29 13:31:46 -0700

    solved in js

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,3 +1,17 @@
 exports.palindrome = function(word) {
+    
+    word = word.toString().toLowerCase();
+    let amswer = "";
 
+    if (word.length % 2 != 0) {
+        let midChar = (word.length / 2) -.5;
+        word = word.replace(word[midChar], "");
+    }
+
+    let firstHalf = word.slice(0, word.length/2).split("").sort().join("");
+    let secondHalf = word.slice(word.length/2).split("").sort().join("");
+    (firstHalf === secondHalf) ? answer = true : answer = false;
+    
+    return answer;
 };
+// could solve bonus part with RegExp and removing whitespace
